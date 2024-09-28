5457855b0afbba9ab38ee522c51d21d1767c499b
Felipeaugusto123 <felipeaugustorodriguesmartins@gmail.com>
2024-09-27 22:51:28 -0300

    First commit

---
+++ src/Entities/Employe.java
@@ -0,0 +1,37 @@
+package Entities;
+
+public class Employe {
+    private Integer id;
+    private String name;
+    private Double salary;
+
+    public Employe(String name, Double salary, Integer id) {
+        this.name = name;
+        this.salary = salary;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
